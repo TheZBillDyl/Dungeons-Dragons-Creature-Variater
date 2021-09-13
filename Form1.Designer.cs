3f450c5853f6029d0a93f7220ca09ff4899c9289
Dylan <45637883+oelkeds21@users.noreply.github.com>
2021-09-12 21:34:31 -0500

    The Beginning

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace DungeonsAndDragonsCreatureVariator
 {
-    partial class Form1
+    partial class mainWindow
     {
         /// <summary>
         /// Required designer variable.
@@ -29,175 +29,80 @@ namespace DungeonsAndDragonsCreatureVariator
         /// </summary>
         private void InitializeComponent()
         {
-            this.btnClickMe = new System.Windows.Forms.Button();
-            this.lblHelloWorld = new System.Windows.Forms.Label();
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
-            this.showPictureButton = new System.Windows.Forms.Button();
-            this.clearButton = new System.Windows.Forms.Button();
-            this.backgroundButton = new System.Windows.Forms.Button();
-            this.closeButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
-            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
-            this.tableLayoutPanel1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            this.flowLayoutPanel1.SuspendLayout();
+            this.openCreatureButton = new System.Windows.Forms.Button();
+            this.selectedCreature = new System.Windows.Forms.Label();
+            this.previousCreatureButton = new System.Windows.Forms.Button();
+            this.nextCreatureButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
-            // 
-            // btnClickMe
-            // 
-            this.btnClickMe.Location = new System.Drawing.Point(136, 104);
-            this.btnClickMe.Name = "btnClickMe";
-            this.btnClickMe.Size = new System.Drawing.Size(75, 23);
-            this.btnClickMe.TabIndex = 0;
-            this.btnClickMe.Text = "Click me";
-            this.btnClickMe.UseVisualStyleBackColor = true;
-            this.btnClickMe.Click += new System.EventHandler(this.button1_Click);
-            // 
-            // lblHelloWorld
-            // 
-            this.lblHelloWorld.AutoSize = true;
-            this.lblHelloWorld.Location = new System.Drawing.Point(154, 88);
-            this.lblHelloWorld.Name = "lblHelloWorld";
-            this.lblHelloWorld.Size = new System.Drawing.Size(33, 13);
-            this.lblHelloWorld.TabIndex = 1;
-            this.lblHelloWorld.Text = "Label";
-            this.lblHelloWorld.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // tableLayoutPanel1
-            // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 15F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 85F));
-            this.tableLayoutPanel1.Controls.Add(this.pictureBox1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.checkBox1, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.flowLayoutPanel1, 1, 1);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 90F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(1102, 612);
-            this.tableLayoutPanel1.TabIndex = 2;
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.tableLayoutPanel1.SetColumnSpan(this.pictureBox1, 2);
-            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.pictureBox1.Location = new System.Drawing.Point(3, 3);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1096, 544);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            // 
-            // checkBox1
-            // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(3, 553);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(60, 17);
-            this.checkBox1.TabIndex = 1;
-            this.checkBox1.Text = "Stretch";
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // flowLayoutPanel1
-            // 
-            this.flowLayoutPanel1.Controls.Add(this.showPictureButton);
-            this.flowLayoutPanel1.Controls.Add(this.clearButton);
-            this.flowLayoutPanel1.Controls.Add(this.backgroundButton);
-            this.flowLayoutPanel1.Controls.Add(this.closeButton);
-            this.flowLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.RightToLeft;
-            this.flowLayoutPanel1.Location = new System.Drawing.Point(168, 553);
-            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
-            this.flowLayoutPanel1.Size = new System.Drawing.Size(931, 56);
-            this.flowLayoutPanel1.TabIndex = 2;
-            // 
-            // showPictureButton
-            // 
-            this.showPictureButton.Location = new System.Drawing.Point(824, 3);
-            this.showPictureButton.Name = "showPictureButton";
-            this.showPictureButton.Size = new System.Drawing.Size(104, 23);
-            this.showPictureButton.TabIndex = 0;
-            this.showPictureButton.Text = "Show Picture";
-            this.showPictureButton.UseVisualStyleBackColor = true;
-            this.showPictureButton.Click += new System.EventHandler(this.showPictureButton_Click);
-            // 
-            // clearButton
-            // 
-            this.clearButton.Location = new System.Drawing.Point(723, 3);
-            this.clearButton.Name = "clearButton";
-            this.clearButton.Size = new System.Drawing.Size(95, 23);
-            this.clearButton.TabIndex = 1;
-            this.clearButton.Text = "Clear picture";
-            this.clearButton.UseVisualStyleBackColor = true;
-            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
-            // 
-            // backgroundButton
-            // 
-            this.backgroundButton.Location = new System.Drawing.Point(595, 3);
-            this.backgroundButton.Name = "backgroundButton";
-            this.backgroundButton.Size = new System.Drawing.Size(122, 23);
-            this.backgroundButton.TabIndex = 2;
-            this.backgroundButton.Text = "Background Color";
-            this.backgroundButton.UseVisualStyleBackColor = true;
-            this.backgroundButton.Click += new System.EventHandler(this.backgroundButton_Click);
-            // 
-            // closeButton
-            // 
-            this.closeButton.Location = new System.Drawing.Point(511, 3);
-            this.closeButton.Name = "closeButton";
-            this.closeButton.Size = new System.Drawing.Size(78, 23);
-            this.closeButton.TabIndex = 3;
-            this.closeButton.Text = "Close";
-            this.closeButton.UseVisualStyleBackColor = true;
-            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // openFileDialog1
             // 
-            this.openFileDialog1.FileName = "openFileDialog1";
-            this.openFileDialog1.Filter = "JPEG Files (*.jpg)|*.jpg|PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|All file" +
-    "s (*.*)|*.*";
-            this.openFileDialog1.Title = "Select a picture file";
+            this.openFileDialog1.FileName = "openCreatureDialog";
+            this.openFileDialog1.Filter = "Excel files (*.xsfx)|*.xsfx|All files (*.*)|*.*";
+            this.openFileDialog1.Title = "Select an Excel file";
             // 
-            // Form1
+            // openCreatureButton
+            // 
+            this.openCreatureButton.Location = new System.Drawing.Point(492, 520);
+            this.openCreatureButton.Name = "openCreatureButton";
+            this.openCreatureButton.Size = new System.Drawing.Size(128, 23);
+            this.openCreatureButton.TabIndex = 0;
+            this.openCreatureButton.Text = "Open Creature";
+            this.openCreatureButton.UseVisualStyleBackColor = true;
+            this.openCreatureButton.Click += new System.EventHandler(this.openCreatureFile_Click);
+            // 
+            // selectedCreature
+            // 
+            this.selectedCreature.AutoSize = true;
+            this.selectedCreature.Location = new System.Drawing.Point(384, 88);
+            this.selectedCreature.Name = "selectedCreature";
+            this.selectedCreature.Size = new System.Drawing.Size(37, 13);
+            this.selectedCreature.TabIndex = 1;
+            this.selectedCreature.Text = "Goblin";
+            // 
+            // previousCreatureButton
+            // 
+            this.previousCreatureButton.Location = new System.Drawing.Point(387, 460);
+            this.previousCreatureButton.Name = "previousCreatureButton";
+            this.previousCreatureButton.Size = new System.Drawing.Size(75, 23);
+            this.previousCreatureButton.TabIndex = 2;
+            this.previousCreatureButton.Text = "Previous";
+            this.previousCreatureButton.UseVisualStyleBackColor = true;
+            this.previousCreatureButton.Click += new System.EventHandler(this.previousCreatureButton_Click);
+            // 
+            // nextCreatureButton
+            // 
+            this.nextCreatureButton.Location = new System.Drawing.Point(668, 460);
+            this.nextCreatureButton.Name = "nextCreatureButton";
+            this.nextCreatureButton.Size = new System.Drawing.Size(75, 23);
+            this.nextCreatureButton.TabIndex = 3;
+            this.nextCreatureButton.Text = "Next";
+            this.nextCreatureButton.UseVisualStyleBackColor = true;
+            this.nextCreatureButton.Click += new System.EventHandler(this.nextCreatureButton_Click);
+            // 
+            // mainWindow
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1102, 612);
-            this.Controls.Add(this.tableLayoutPanel1);
-            this.Controls.Add(this.lblHelloWorld);
-            this.Controls.Add(this.btnClickMe);
-            this.Name = "Form1";
-            this.Text = "Picture Viewer";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.tableLayoutPanel1.PerformLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            this.flowLayoutPanel1.ResumeLayout(false);
+            this.Controls.Add(this.nextCreatureButton);
+            this.Controls.Add(this.previousCreatureButton);
+            this.Controls.Add(this.selectedCreature);
+            this.Controls.Add(this.openCreatureButton);
+            this.Name = "mainWindow";
+            this.Text = "Creature Variator";
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button btnClickMe;
-        private System.Windows.Forms.Label lblHelloWorld;
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.CheckBox checkBox1;
-        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
-        private System.Windows.Forms.Button showPictureButton;
-        private System.Windows.Forms.Button clearButton;
-        private System.Windows.Forms.Button backgroundButton;
-        private System.Windows.Forms.Button closeButton;
         private System.Windows.Forms.OpenFileDialog openFileDialog1;
-        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button openCreatureButton;
+        private System.Windows.Forms.Label selectedCreature;
+        private System.Windows.Forms.Button previousCreatureButton;
+        private System.Windows.Forms.Button nextCreatureButton;
     }
 }
 
